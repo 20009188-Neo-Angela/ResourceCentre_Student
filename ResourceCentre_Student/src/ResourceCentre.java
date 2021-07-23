22184544dd56115803cc12152e2c3fb160183306
20023613 <20023613@DESKTOP-J8QI278>
2021-07-23 12:53:21 +0800

    # WARNING: head commit changed in the meantime
    trying YT

--- ResourceCentre_Student/src/ResourceCentre.java
+++ ResourceCentre_Student/src/ResourceCentre.java
@@ -146,7 +146,7 @@ public class ResourceCentre {
 
 	public static String retrieveAllChromebook(ArrayList<Chromebook> chromebookList) {
 		String output = "";
-		// write your code here
+		// write your code here please!
 		return output;
 	}
 	public static void viewAllChromebook(ArrayList<Chromebook> chromebookList) {
