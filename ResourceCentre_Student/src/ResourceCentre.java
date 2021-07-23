becd702097bc491087994115ba55484f1142ed36
User <User@DESKTOP-J8QI278>
2021-07-23 12:56:21 +0800

    yt test

--- ResourceCentre_Student/src/ResourceCentre.java
+++ ResourceCentre_Student/src/ResourceCentre.java
@@ -147,6 +147,7 @@ public class ResourceCentre {
 	public static String retrieveAllChromebook(ArrayList<Chromebook> chromebookList) {
 		String output = "";
 		// write your code here please!!
+		// YT
 		return output;
 	}
 	public static void viewAllChromebook(ArrayList<Chromebook> chromebookList) {
